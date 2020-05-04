89af80d88512f790a39cffcf70b63f65d04b7607
Andrew Wormald <andreww@luno.com>
2020-05-04 14:15:25 +0200

    refactor

--- backend-feed/src/server.ts
+++ backend-feed/src/server.ts
@@ -14,13 +14,13 @@ const c = config.dev;
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080; // default port to listen
-  
+  const port = process.env.FEED_PORT || 8200; // default port to listen
+
   app.use(bodyParser.json());
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", c.url);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -31,11 +31,11 @@ const c = config.dev;
   app.get( "/", async ( req, res ) => {
     res.send( "/api/v0/" );
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running ` + c.url );
+      console.log( `server running on: ` + port );
       console.log( `press CTRL+C to stop server` );
   } );
 })();
