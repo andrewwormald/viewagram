d25040c5dd91e00d63850f5dac65483e72bb785d
Andrew Wormald <andreww@luno.com>
2020-05-08 10:18:02 +0200

    update ports

--- backend-user/src/server.ts
+++ backend-user/src/server.ts
@@ -14,7 +14,7 @@ const c = config.dev;
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.USER_PORT || 8300; // default port to listen
+  const port = process.env.USER_PORT || 8080; // default port to listen
 
   app.use(bodyParser.json());
 
